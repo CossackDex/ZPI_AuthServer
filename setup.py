c9f8d53c681e186c14d409bd7e241814a9e14a3f
dexon44 <242589@student.pwr.edu.pl>
2020-04-11 03:44:37 +0200

    Init Commit v2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ZPI_AuthServer',
+    version='v1',
+    packages=['auth_server_application'],
+    url='',
+    license='MIT',
+    author='Student242589',
+    author_email='d.matuszczyk98@gmail.com',
+    description=''
+)
